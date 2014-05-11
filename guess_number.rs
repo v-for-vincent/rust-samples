c8829b45fd3ae6d10366100c5d1aa73cbe3fcfcc
Vincent Nys <vincentnys@gmail.com>
2014-05-11 15:17:42 +0200

    Compiles, but still contains warnings. No EOF functionality yet.

--- guess_number.rs
+++ guess_number.rs
@@ -6,9 +6,9 @@ use std::io;
 
 fn main() {
     println!("I'm thinking of a number between 1 and 100. Can you guess it?");
-    let x = random();
+    let x = random::<int>();
     let mut reader = BufferedReader::new(io::stdin());
-    while !reader.eof() {
+    while true {
         let line = reader.read_line().unwrap();
         let num = from_str::<int>(line.slice_to(line.len() - 1));
         match num {
