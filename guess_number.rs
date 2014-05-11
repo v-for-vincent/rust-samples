bb392703e89c154b618e81ca199ccf5eb20b8a80
Vincent Nys <vincentnys@gmail.com>
2014-05-11 15:11:28 +0200

    Looking for a way to recognize EOF correctly.

---
+++ guess_number.rs
@@ -0,0 +1,19 @@
+extern crate rand;
+
+use rand::random;
+use std::io::BufferedReader;
+use std::io;
+
+fn main() {
+    println!("I'm thinking of a number between 1 and 100. Can you guess it?");
+    let x = random();
+    let mut reader = BufferedReader::new(io::stdin());
+    while !reader.eof() {
+        let line = reader.read_line().unwrap();
+        let num = from_str::<int>(line.slice_to(line.len() - 1));
+        match num {
+            Some(number_string) => println!("Well, it was a number."),
+            None                => println!("Doesn't count.")
+        }
+    }
+}
